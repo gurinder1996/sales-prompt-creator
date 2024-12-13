e4fa91f9f4714eb88f9adc44fc67cbf053086171
John George <askjohngeorge@gmail.com>
2024-12-13 23:46:57 +0000

    Merge social-links.tsx into site-footer.tsx so I have a simple footer I can copy across to other projects

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,4 +1,27 @@
-import SocialLinks from "./social-links";
+import { FaTwitter, FaLinkedin, FaGithub, FaYoutube } from "react-icons/fa";
+
+function SocialLinks({ className }: { className?: string }) {
+  return (
+    <div className={`flex ${className}`}>
+      {[
+        { icon: FaYoutube, href: "https://youtube.com/@askjohngeorge" },
+        { icon: FaLinkedin, href: "https://linkedin.com/in/askjohngeorge"},
+        { icon: FaGithub, href: "https://github.com/askjohngeorge" },
+        { icon: FaTwitter, href: "https://x.com/askjohngeorge" },
+      ].map((social, index) => (
+        <span key={index}>
+          <a href={social.href} target="_blank" rel="noopener noreferrer">
+            <social.icon
+              size={24}
+              className="text-white transition-all duration-200 mx-2 ease-in-out hover:text-background hover:scale-125"
+            />
+          </a>
+        </span>
+      ))}
+    </div>
+  );
+}
+
 
 export function SiteFooter() {
   return (
